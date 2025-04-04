6694c9986e724f11d65fcc6547a28abbb3faab3c
Carrow31600 <cminguez31@gmail.com>
2025-04-04 18:39:50 +0200

    add presentation and cover

---
+++ frontend/src/app/home/cover/cover.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cover',
+  imports: [],
+  templateUrl: './cover.component.html',
+  styleUrl: './cover.component.css',
+  standalone : true,
+})
+export class CoverComponent {
+
+}

a2cd828a5814d458b5f41bc2f0a30403f01f3b26
Carrow31600 <cminguez31@gmail.com>
2025-04-05 09:14:31 +0200

    add events

--- frontend/src/app/home/home/home.component.ts
+++ frontend/src/app/home/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CoverComponent } from '../cover/cover.component';
 import { PresentationComponent } from '../presentation/presentation.component';
+import { EventsComponent } from '../events/events.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CoverComponent, PresentationComponent],
+  imports: [CoverComponent, PresentationComponent,EventsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone : true,

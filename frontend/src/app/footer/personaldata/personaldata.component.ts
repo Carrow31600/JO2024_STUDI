a6e211bde14ebdc8c1f0e92ea9687a2a560344b7
Carrow31600 <cminguez31@gmail.com>
2025-04-05 17:50:14 +0200

    add cookies and date component

---
+++ frontend/src/app/footer/personaldata/personaldata.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personaldata',
+  imports: [],
+  templateUrl: './personaldata.component.html',
+  styleUrl: './personaldata.component.css'
+})
+export class PersonaldataComponent {
+
+}

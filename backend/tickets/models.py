35515f13b2b776d7551b4fddd8f301a0acceb0ff
Carrow31600 <cminguez31@gmail.com>
2025-04-18 16:12:08 +0200

    add qrcode

--- backend/tickets/models.py
+++ backend/tickets/models.py
@@ -6,7 +6,9 @@ from orders.models import Order
 class Ticket(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     ticketkey = models.CharField(max_length=100, unique=True)
-    status = models.BooleanField(default=False)   
+    status = models.BooleanField(default=False) 
+    qr_code = models.ImageField(upload_to='qr_codes/', blank=True, null=True) 
+      
 
     def __str__(self):
         return f"Ticket for Order #{self.order.pk} with key {self.ticketkey}"
--- backend/tickets/signals.py
+++ backend/tickets/signals.py
@@ -1,7 +1,11 @@
+from io import BytesIO
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import qrcode
 from orders.models import Order
 from .models import Ticket
+from django.core.files import File
+from django.core.files.base import ContentFile 
 
 @receiver(post_save, sender=Order)
 def create_ticket_on_payment(sender, instance, **kwargs):
@@ -13,4 +17,18 @@ def create_ticket_on_payment(sender, instance, **kwargs):
         ticketkey = f"{userkey}-{orderkey}"
 
         # Créer le ticket
-        Ticket.objects.create(order=instance, ticketkey=ticketkey)
+        print("Signal reçu : création de ticket")
+        ticket = Ticket.objects.create(order=instance, ticketkey=ticketkey)
+
+
+        # Générer le QR code
+        qr_img = qrcode.make(ticketkey)
+        buffer = BytesIO()
+        qr_img.save(buffer, format='PNG')
+
+        # Enregistrer le fichier dans le champ ImageField
+        file_name = f"qr_{ticketkey}.png"
+        ticket.qr_code.save(file_name, ContentFile(buffer.getvalue()), save=False)
+
+        ticket.save()
+        print("QR code généré et ticket enregistré")
